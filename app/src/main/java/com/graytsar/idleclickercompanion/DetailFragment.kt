3379288473f26a2837f4e78a1b164b3db069c36c
GrayTsar <viatcheslav.ehrmann@gmail.com>
2020-03-13 23:10:04 +0100

    Fr and Kr

--- app/src/main/java/com/graytsar/idleclickercompanion/DetailFragment.kt
+++ app/src/main/java/com/graytsar/idleclickercompanion/DetailFragment.kt
@@ -1,7 +1,6 @@
 package com.graytsar.idleclickercompanion
 
 import android.app.AlertDialog
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -9,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.ItemTouchHelper
--- app/src/main/java/com/graytsar/idleclickercompanion/HomeFragment.kt
+++ app/src/main/java/com/graytsar/idleclickercompanion/HomeFragment.kt
@@ -9,7 +9,6 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
